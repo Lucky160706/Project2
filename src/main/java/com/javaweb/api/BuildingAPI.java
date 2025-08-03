5b4cb603eb970230cc92831f54d8362cfeeb8e49
mycode-Quoc-Do <QuocPhu.Do@seattlecolleges.edu>
2025-08-02 23:42:51 -0700

    update project

---
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -0,0 +1,46 @@
+package com.javaweb.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaweb.model.BuildingDTO;
+import com.javaweb.service.BuildingService;
+
+import customexception.FieldRequiredException;
+
+@RestController
+public class BuildingAPI {
+	
+	@Autowired
+	private BuildingService buildingService;
+	@GetMapping(value = "/api/building/")
+	public List<BuildingDTO> getBuilding(@RequestParam(name = "name", required = false) String name,
+									     @RequestParam(name = "districtid", required = false) Long district,
+									     @RequestParam(name = "typeCode", required = false) List<String> typeCode) {
+		
+		List<BuildingDTO> result = buildingService.findAll(name, district);
+		return result; 
+		
+	}
+	
+	public void validate(BuildingDTO buildingDTO) throws  FieldRequiredException {
+		if (buildingDTO.getName() == null 
+				|| buildingDTO.getName().equals("")
+				|| buildingDTO.getNumberOfBasement() == null) {
+			throw new FieldRequiredException("name or numberofbasement is null");
+		}
+	}
+	
+	@DeleteMapping(value = "/api/building/{id}/{name}")
+	public void deleteBuilding(@PathVariable Integer id, 
+							   @PathVariable String name,
+							   @RequestParam(value = "ward", required = false) String ward) {
+		System.out.println("Đã xoá toà nhà có id là " + id + " và " + name + ", " + ward+ " rồi ");
+	}
+} 
